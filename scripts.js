f46c3e74a16cd2b36b9aa43dd105035a9ee836ab
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-05-16 21:35:28 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -79,7 +79,7 @@ async function fetchAndRenderProducts() {
       const div = document.createElement('div');
       div.className = 'product';
       div.innerHTML = `
-        <img src="${product.image}" alt="${product.name}" />
+        <img src="${product.images}" alt="${product.name}" />
         <h3>${product.name}</h3>
         <p>${product.price.toLocaleString()}₫</p>
         <button class="add-to-cart"
